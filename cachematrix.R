40c31976705fbc9a17728c3c228917011dcf3787
Defcom <opayan@yahoo.com>
2015-06-20 21:47:56 -0500

    test

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,16 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix2 <- function(x = matrix()) {
+  m <- x$getmatrix ()
+  if (!is.null(m)){
+    message("getting cache")
+    return (m)
+  }
+  data <- x$get()
+  m <- solve(data)
+  x$setmatrix(m)
+  m
 }
 
 
@@ -12,4 +20,34 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if (!is.null(m)){
+    message("getting cache")
+    return (m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
+  
+}
+
+makeCacheMatrix3 <- function(x = matrix(), size) {
+  xR <- runif(size*size, 1, 100)
+  x <- matrix(xR, nrow = size, ncol = size)
+  
+}
+
+
+makeCacheMatrix <- function(x = matrix() ) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve(x)
+  getsolve <- function() m
+  list(set = set, get = get, setsolve = setsolve, getsolve = getsolve)
+  
 }
